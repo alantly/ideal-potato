5e8f1c637ade3b3ecb7104f67c9af32c70e9107f
Alan <alanly211@gmail.com>
2017-07-01 13:42:39 -0700

    export basic using an index.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import * as expect from 'expect'
-import { add } from './lib/math/basic/add';
-import { subtract } from './lib/math/basic/subtract';
+import { add, subtract } from './lib/math/basic';
 import { squareArea } from './lib/math/geometry/square-area';
 
 expect(add(5,6)).toBe(11);
