e100c474ecdec666172100459c301fdfd610b61e
Alan <alanly211@gmail.com>
2017-07-01 13:46:20 -0700

    export geometry and add index to math to export math as a single module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import * as expect from 'expect'
-import { add, subtract } from './lib/math/basic';
-import { squareArea } from './lib/math/geometry/square-area';
+import { add, subtract, squareArea } from './lib/math';
 
 expect(add(5,6)).toBe(11);
 expect(subtract(5,6)).toBe(-1);
